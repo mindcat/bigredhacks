c0a52a4de7b0b5cf89da3bb20ba2741876236ccb
aln4739 <henryy20022@gmail.com>
2024-10-05 13:45:56 -0400

    added nativewind css

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,39 +2,10 @@ import { Text, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <MapView
-        // provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-      {/* <Text>Edit app/index.tsx to edit this.</Text> */}
+    <View>
+      <MapView className="h-full w-full" />
     </View>
   );
 }
