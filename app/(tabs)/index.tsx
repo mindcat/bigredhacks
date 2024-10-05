410bb7af1e58a4abbd2c373ba737647df93c870f
aln4739 <henryy20022@gmail.com>
2024-10-05 17:05:11 -0400

    know how to do markers now

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,31 @@
-import { Text, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, Pressable, View } from "react-native";
+import MapView, { MapMarkerProps, Marker } from "react-native-maps";
 
 export default function Index() {
+  const [markers, setMarkers] = useState([
+    {
+      coordinate: { latitude: 37.78825, longitude: -122.4324 },
+      title: "Hello",
+      description: "World",
+    },
+  ]);
+
   return (
     <View>
-      <MapView className="h-full w-full" />
+      <MapView className="h-full" tintColor="red">
+        {markers.map((marker: MapMarkerProps, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+      <Pressable className="h-8 ">
+        <Text className="text-white text-center">Add Marker</Text>
+      </Pressable>
     </View>
   );
 }
